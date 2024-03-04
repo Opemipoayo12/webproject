7d1906f8bdb8a0387f6c9dd87ea59d5832b94500
Opemipoayo12 <162073818+Opemipoayo12@users.noreply.github.com>
2024-03-03 23:44:45 -0500

    Update sccript.js

--- sccript.js
+++ sccript.js
@@ -1,4 +1,4 @@
-// Function to handle opening and closing the mobile menu
+
 function myMenuFunction() {
     var menu = document.getElementById("myNavMenu");
     if (menu.style.display === "block") {
@@ -8,28 +8,21 @@ function myMenuFunction() {
     }
 }
 
-// Function to handle form submission
+
 function handleSubmit(event) {
-    event.preventDefault(); // Prevent the form from submitting
-    // Get form inputs
+    event.preventDefault(); 
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    // You can perform further validation here if needed
-
-    // For demonstration purposes, log the form data to the console
     console.log("Name:", name);
     console.log("Email:", email);
     console.log("Message:", message);
 
-    // Clear the form inputs after submission
     document.getElementById('name').value = '';
     document.getElementById('email').value = '';
     document.getElementById('message').value = '';
 
-    // Optionally, you can add code to send the form data to a backend server or perform other actions
+ 
 }
-
-// Attach the handleSubmit function to the form submission event
 document.getElementById('contact-form').addEventListener('submit', handleSubmit);
